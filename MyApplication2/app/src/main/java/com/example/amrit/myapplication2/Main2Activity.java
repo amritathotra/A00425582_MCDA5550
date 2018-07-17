31cc5c65c7fc009646f6a7764831e5ba97135872
Amrit Singh <name@names-MacBook-Air.local>
2018-07-17 19:15:35 -0300

    calculation button working
    calculation button working

--- MyApplication2/app/src/main/java/com/example/amrit/myapplication2/Main2Activity.java
+++ MyApplication2/app/src/main/java/com/example/amrit/myapplication2/Main2Activity.java
@@ -3,6 +3,9 @@ package com.example.amrit.myapplication2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Main2Activity extends AppCompatActivity {
@@ -11,6 +14,39 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button cal=(Button) findViewById(R.id.buttonCal);
+        cal.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                calculate();
+            }
+        });
     }
 
+    public void calculate(){
+        EditText height = (EditText) findViewById(R.id.textHeight);
+        String value = height.getText().toString();
+        Double heightVal = Double.parseDouble(value);
+        System.out.println("here is the height "+heightVal);
+
+        //repeat for weight
+        EditText weight = (EditText) findViewById(R.id.textWeight);
+        String wvalue = height.getText().toString();
+        Double weightVal = Double.parseDouble(wvalue);
+        System.out.println("here is the height "+weightVal);
+
+        Double calc = (weightVal/(heightVal*heightVal));
+        EditText result = (EditText) findViewById(R.id.result);
+
+        result.setText(Double.toString(calc));
+    }
+
+    /*public void onClickEnter(View view){
+        Intent intent = new Intent( this,Main2Activity.class);
+        startActivity(intent);
+
+    }*/
 }
--- MyApplication2/app/src/main/java/com/example/amrit/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/amrit/myapplication2/MainActivity.java
@@ -13,20 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void calculate(){
-        EditText height = (EditText) findViewById(R.id.textHeight);
-        String value = height.getText().toString();
-        Double heightVal = Double.parseDouble(value);
-        System.out.println("here is the height "+heightVal);
-
-        //repeat for weight
-        Double calc = (weightVal/(heightVal*heightVal));
-        EditText result = (EditText) findViewById(R.id.result);
-
-        result.setText(calc);
-    }
     public void onClickEnter(View view){
         Intent intent = new Intent( this,Main2Activity.class);
         startActivity(intent);
+
     }
 }
